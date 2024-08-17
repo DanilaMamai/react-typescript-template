ee1fe090259cad36c4763f70dca9ed109e22af51
Мамаенко Даниил Евгеньевич <de_mamaenko@betboom.org>
2024-08-17 20:49:00 +0300

    main

--- webpack/utils.ts
+++ webpack/utils.ts
@@ -4,13 +4,23 @@ import webpack from "webpack";
 import { WebpackOptions } from "./types";
 
 const buildLoaders = (options: WebpackOptions): webpack.ModuleOptions => {
+  const fontsLoader = {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: 'asset/resource',
+  };
+
+  const imagesLoader = {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+  };
+
   const tsLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
 
-  return { rules: [tsLoader] };
+  return { rules: [tsLoader, fontsLoader, imagesLoader] };
 };
 
 
